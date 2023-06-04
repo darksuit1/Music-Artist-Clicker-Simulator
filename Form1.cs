bb4a2b31b4f3cd2114160951f5e45ba5c2ce740a
darksuit <densergalay@gmail.com>
2023-06-04 22:57:20 +0300

    comments2
    Добавил еще комментарии

--- Form1.cs
+++ Form1.cs
@@ -22,7 +22,7 @@ namespace Music_Artist
         private int remainingTime = 60;
         private int dollars = 0;
 
-        public class Instrument
+        public class Instrument // Создаем класс Музыкальных инструментов
         {
             public string Name { get; set; }
             public int Price { get; set; }
@@ -43,9 +43,9 @@ namespace Music_Artist
             }
         }
 
-        private List<Instrument> instruments = new List<Instrument>()
+        private List<Instrument> instruments = new List<Instrument>() // Создаем лист инструментов
         {
-            new Instrument("Гитара", 1, false, 4),
+            new Instrument("Гитара", 1, false, 4), // Это сами инструменты, последнее число отвечает за то сколько очков будет давать за выпущенный трек
             new Instrument("Барабаны", 2, false, 1),
             new Instrument("Клавишные", 3, false, 2)
         };
@@ -54,7 +54,7 @@ namespace Music_Artist
         {
             InitializeComponent();
             usinglabel.Text = $"Лейбл: {currentLabel}";
-            listBox1.Items.AddRange(instruments.ToArray());
+            listBox1.Items.AddRange(instruments.ToArray()); // Добавляем инструменты в листбокс(Магазин)
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -64,15 +64,15 @@ namespace Music_Artist
 
         private void clicker_Click(object sender, EventArgs e)
         {
-            guna2ProgressBar1.Value = buttonCount++;
-            label1.Text = $"Очки: {score}";
-            if (buttonCount == 50)
+            guna2ProgressBar1.Value = buttonCount++; // Каждое нажатие кнопки заполняет прогрессбар на 1 процент
+            label1.Text = $"Очки: {score}"; // Выводит кол-во очков
+            if (buttonCount == 50) // После 50 кликов становится доступна кнопка для выпуска треков
             {
                 releasetrack.Enabled = true;
             }
         }
 
-        private void releasetrack_Click(object sender, EventArgs e)
+        private void releasetrack_Click(object sender, EventArgs e) // Кнопка которая выпускает трек
         {
             tracksReleased++;
 
@@ -82,32 +82,32 @@ namespace Music_Artist
             {
                 if (instrument.Bought)
                 {
-                    bonus += instrument.Points;
+                    bonus += instrument.Points; // Если у игрока есть инструменты в инвентаре то ему за музыкальные инструменты начисляются очки при выпуске треков
                 }
             }
 
-            if (currentLabel != "нет лейбла")
+            if (currentLabel != "нет лейбла") // Если нет лейбла то игроку не будет начисляться +10 очков
             {
                 score += 20 +bonus;
-                MessageBox.Show($"Трек выпущен! Добавлено 20 очков (включая бонусы).");
+                MessageBox.Show($"Трек выпущен! Добавлено 20 очков (может включать бонусы).");
             }
             else
             {
                 score += 10 + bonus;
-                MessageBox.Show($"Трек выпущен! Добавлено 10 очков.(может включать бонусы)");
+                MessageBox.Show($"Трек выпущен! Добавлено 10 очков.(может включать бонусы)"); // тут мы по стандарту начисляем 10 очков игроку за каждый выпущенный трек
             }
             guna2ProgressBar1.Value = 0;
             buttonCount = 0;
             releasetrack.Enabled = false;
-            label1.Text = $"Очки: {score}";
-            label2.Text = $"Выпущено треков: {tracksReleased}";
+            label1.Text = $"Очки: {score}"; // Тут выводим количество очков
+            label2.Text = $"Выпущено треков: {tracksReleased}"; // Тут выводим количество выпущенных треков
         }
 
-        private void timer1_Tick(object sender, EventArgs e)
+        private void timer1_Tick(object sender, EventArgs e) // Тут мы запускаем таймер если игрок вступил в лейбл
         {
             remainingTime--;
             rmtime.Text = $"Оставшееся время: {remainingTime} сек";
-            if (remainingTime == 0)
+            if (remainingTime == 0) // останавливаем таймер если время в музыкальном лейбле истекло
             {
                 timer1.Stop();
                 currentLabel = "нет лейбла";
@@ -117,7 +117,7 @@ namespace Music_Artist
             }
         }
 
-        private void findlabel_Click(object sender, EventArgs e)
+        private void findlabel_Click(object sender, EventArgs e) // Кнопка открытия формы для поиска лейбла
         {
             Form2 form2 = new Form2();
             var result = form2.ShowDialog();
@@ -130,9 +130,9 @@ namespace Music_Artist
             }
         }
 
-        private void guna2Button1_Click(object sender, EventArgs e)
+        private void guna2Button1_Click(object sender, EventArgs e) 
         {
-            if (int.TryParse(guna2TextBox1.Text, out int dollars))
+            if (int.TryParse(guna2TextBox1.Text, out int dollars)) // Конвертируем очки в деньги
             {
                 int points = dollars * 100;
                 if (score >= points)
@@ -145,13 +145,13 @@ namespace Music_Artist
                 }
                 else
                 {
-                    MessageBox.Show($"Недостаточно очков! Необходимо {points} очков для конвертации {dollars} долларов.",
+                    MessageBox.Show($"Недостаточно очков! Необходимо {points} очков для конвертации {dollars} долларов.", // если у игрока меньше 100 очков то ему не начислится доллар (100 очков = 1$)
                         "Неудача");
                 }
             }
             else
             {
-                MessageBox.Show("Введите правильное количество долларов.", "Ошибка!");
+                MessageBox.Show("Введите правильное количество долларов.", "Ошибка!"); // Если игрок ввел недопустимое количество
             }
         }
 
@@ -160,7 +160,7 @@ namespace Music_Artist
 
         }
 
-        private void guna2Button3_Click(object sender, EventArgs e)
+        private void guna2Button3_Click(object sender, EventArgs e) // Кнопка для покупки музыкальных инструментов
         {
             if (listBox1.SelectedItem != null)
             {
@@ -169,22 +169,22 @@ namespace Music_Artist
                 {
                     if (dollars >= instrument.Price)
                     {
-                        dollars -= instrument.Price;
-                        money.Text = $"Доллары: {dollars.ToString("C2")}";
+                        dollars -= instrument.Price; // Забираем у игрока стоимость инструмента
+                        money.Text = $"Доллары: {dollars.ToString("C2")}"; // обновляем баланс
                         instrument.Bought = true;
                         listBox1.Items[listBox1.SelectedIndex] = instrument;
-                        listBox2.Items.Add(instrument.Name);
-                        MessageBox.Show($"Поздравляем! Вы купили инструмент: {instrument.Name}.", "Покупка завершена");
+                        listBox2.Items.Add(instrument.Name); // Добавляем купленный инструмент в инвентарь
+                        MessageBox.Show($"Поздравляем! Вы купили инструмент: {instrument.Name}.", "Покупка завершена"); 
                     }
                     else
                     {
-                        MessageBox.Show($"Недостаточно долларов! Необходимо {instrument.Price} долларов для покупки инструмента: {instrument.Name}.",
+                        MessageBox.Show($"Недостаточно долларов! Необходимо {instrument.Price} долларов для покупки инструмента: {instrument.Name}.", // Если у игрока нету денег то ему не даст купить инструмент
                             "Неудача");
                     }
                 }
                 else
                 {
-                    MessageBox.Show($"Инструмент: {instrument.Name} уже куплен!",
+                    MessageBox.Show($"Инструмент: {instrument.Name} уже куплен!", // Если инструмент уже куплен
                         "Информация");
                 }
             }
@@ -195,7 +195,7 @@ namespace Music_Artist
             }
         }
 
-        private void guna2Button4_Click(object sender, EventArgs e)
+        private void guna2Button4_Click(object sender, EventArgs e) // Кнопка удаления инструмента из инвентаря
         {
             if (listBox2.SelectedItem != null)
             {
@@ -203,9 +203,9 @@ namespace Music_Artist
                 Instrument instrument = instruments.Find(i => i.Name == instrumentName);
                 if (instrument != null)
                 {
-                    instrument.Bought = false;
+                    instrument.Bought = false; // Делаем инструмент неприобретенным
                     listBox1.Items[listBox1.SelectedIndex] = instrument;
-                    listBox2.Items.Remove(instrumentName);
+                    listBox2.Items.Remove(instrumentName); // Удаляем из инвентаря
                     MessageBox.Show($"Инструмент: {instrumentName} удален из вашего инвентаря.", "Успех");
                 }
             }
@@ -216,14 +216,14 @@ namespace Music_Artist
             }
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e) // Кнопка для быстрого начисления баллов чтобы проверить игру если лень зарабатывать очки. По умолчанию кнопка скрыта
         {
             score += 100;
         }
 
         private void guna2CircleButton1_Click(object sender, EventArgs e)
         {
-            Application.Exit();
+            Application.Exit(); // Закрываем игру
         }
 
         private void guna2Button2_Click(object sender, EventArgs e)
